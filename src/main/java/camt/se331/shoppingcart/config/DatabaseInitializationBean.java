a96d01b6f348bfe00867e64cde335a9d588f56aa
PREMPREMCNX <premprem.cnx@gmail.com>
2015-02-25 01:40:46 +0700

    <4.5.2.1> Add @Component

--- src/main/java/camt/se331/shoppingcart/config/DatabaseInitializationBean.java
+++ src/main/java/camt/se331/shoppingcart/config/DatabaseInitializationBean.java
@@ -14,6 +14,7 @@ import java.util.Arrays;
  */
 
 @Profile("db.init")
+@Component
 public class DatabaseInitializationBean implements InitializingBean {
     @Autowired
     ProductRepository productRepository;
